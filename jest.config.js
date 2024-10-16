b798b2c8dfafc41451d85513cc6372326914dfcd
Nick <teamnescafe@yandex.ru>
2024-10-16 13:38:40 +0300

    .

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-        '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
-    },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-};
